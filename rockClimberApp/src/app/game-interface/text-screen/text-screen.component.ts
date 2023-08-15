f9a50332babd8ed1d7688caed717205b8c45f6a9
Long Bui <Long.Bui@willistowerswatson.com>
2023-08-15 14:44:29 +0100

    fixed spacing issue

--- rockClimberApp/src/app/game-interface/text-screen/text-screen.component.ts
+++ rockClimberApp/src/app/game-interface/text-screen/text-screen.component.ts
@@ -3,15 +3,12 @@ import { Component, Input } from '@angular/core';
   selector: 'app-text-screen',
   template: `
   <div class="textScreen" #textScreen>
-
     <span
       *ngFor="let char of textArray; let index = index"
       [class.incomplete]="!userInput[index]"
       [class.incorrect]="userInput[index] && userInput[index] !== char"
       [class.correct]="userInput[index] === char"
-    >
-      {{ char }}
-    </span>
+    >{{char}}</span>
   </div>
 
   `,
