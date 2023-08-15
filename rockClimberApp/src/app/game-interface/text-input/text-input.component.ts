ed0bca71c9a7b19c9dc9a2794489220b37804a50
Long Bui <Long.Bui@willistowerswatson.com>
2023-08-15 16:35:50 +0100

    detect space is pressed

--- rockClimberApp/src/app/game-interface/text-input/text-input.component.ts
+++ rockClimberApp/src/app/game-interface/text-input/text-input.component.ts
@@ -3,15 +3,24 @@ import { Component, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-text-input',
   template: `
-    <input type="text" [(ngModel)]="inputText" (input)="onInputChange()">
+    <input type="text" [(ngModel)]="inputText" (input)="onInputChange()" (keypress)="onKeyPressEvent($event)">
   `,
   styles: []
 })
 export class TextInputComponent{
+
   inputText!: string;
   @Output() inputChange = new EventEmitter<string>();
 
   onInputChange() {
     this.inputChange.emit(this.inputText);
   }
+
+  onKeyPressEvent(event: KeyboardEvent) {
+    if (event.key === ' ') {
+      // Your code to execute when space is pressed
+      console.log('Space key pressed!');
+    }
+  }
+
 }
