fcb5da5143e8a0912f840471ef811ef7a71a3b51
Long Bui <Long.Bui@willistowerswatson.com>
2023-08-17 10:13:37 +0100

    better names

--- rockClimberApp/src/app/game-interface/text-input/text-input.component.ts
+++ rockClimberApp/src/app/game-interface/text-input/text-input.component.ts
@@ -3,7 +3,7 @@ import { Component, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-text-input',
   template: `
-    <input type="text" [(ngModel)]="inputText" (input)="onInputChange()" (keypress)="onKeyPressEvent($event)">
+    <input type="text" [(ngModel)]="inputText" (input)="emitUserInput()" (keypress)="onKeyPressEvent($event)">
   `,
   styles: []
 })
@@ -12,14 +12,40 @@ export class TextInputComponent{
   inputText!: string;
   @Output() inputChange = new EventEmitter<string>();
 
-  onInputChange() {
+  emitUserInput() {
     this.inputChange.emit(this.inputText);
   }
 
   onKeyPressEvent(event: KeyboardEvent) {
     if (event.key === ' ') {
-      // Your code to execute when space is pressed
-      console.log('Space key pressed!');
+
+      //convert to angular:
+      const spanArray = quoteDisplayElement.querySelectorAll('span.incomplete')
+      let currentWord = currentText.wordArray[currentText.arrayIndex] //get curent word (no spaces)
+      const lastWord= currentText.wordArray[currentText.wordArray.length - 1]
+      const userInput=quoteInputElement.value
+
+      if(userInput == currentWord && userInput != lastWord){
+          currentText.progressBar= move(currentText.progressBar,userInput,currentText.charArray)
+          e.preventDefault();
+          
+
+          const completedChars= userInput.split("")
+          for(let i=0;i<completedChars.length+1;i++){ //add 1 to length so it includes the space as well.
+              spanArray[i].classList.remove("incomplete") //removes class off the first # of nodelist.
+              spanArray[i].classList.add("correct")
+          }
+
+          currentText.arrayIndex++;
+          //move to next word in array
+          currentWord = currentText.wordArray[currentText.arrayIndex]
+          console.log(`array index: ${currentText.arrayIndex}`)
+          quoteInputElement.value=""
+
+      }else if(userInput==lastWord){
+          const elem = document.getElementById("racer");
+          elem.style.width=`100%`
+      }
     }
   }
 
