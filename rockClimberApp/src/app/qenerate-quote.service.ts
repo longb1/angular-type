207141d4071cebe1d536897f5f09534b208bda22
Long Bui <Long.Bui@willistowerswatson.com>
2023-08-17 13:13:46 +0100

    cleanup

--- rockClimberApp/src/app/qenerate-quote.service.ts
+++ rockClimberApp/src/app/qenerate-quote.service.ts
@@ -10,14 +10,6 @@ export class GenerateQuoteService {
 
   constructor(private http: HttpClient) { }
 
-  // getRandomQuote() {
-  //   this.http.get(this.RANDOM_QUOTE_API_URL).subscribe((response: any) => {
-  //     const content = response.content; // Extract the 'content' property from the response
-  //     // Use the 'content' variable as needed
-  //     return content.toString()
-  //   });
-  // }
-
   getRandomQuote(): Observable<string> {
     return this.http.get<{content:string}>(this.RANDOM_QUOTE_API_URL).pipe(
       map(response => response.content)
