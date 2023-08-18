81d79102bca323dc32b8356cc349aff3e219911d
Long Bui <Long.Bui@willistowerswatson.com>
2023-08-18 16:03:48 +0100

    new object

--- rockClimberApp/src/app/game-interface/game-interface.component.ts
+++ rockClimberApp/src/app/game-interface/game-interface.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { GenerateQuoteService } from 'src/app/qenerate-quote.service';
+import { IQuoteObject } from '../quote-object';
 
 @Component({
   selector: 'app-game-interface',
@@ -10,26 +11,28 @@ export class GameInterfaceComponent {
   constructor(private generateQuoteService : GenerateQuoteService) { }
 
   receivedInput: string[] = [];
-  displayText:string[] = [];
-  currentWord?:string;
-  wordArray:string[] = [];
-  
+  quoteObject: IQuoteObject = {
+    charArray: [],
+    wordArray: [],
+    indexProgress: 0,
+    userProgress: ''
+  };
+
   sendInput(text: string) {
     this.receivedInput = text.split('');
   }
 
-  //two way between text-input to get current user input
-  //compare with word array and dynamically set displayText for display component
-  //first: get user input emitted to this component.
-  compare(text:string){
-    this.currentWord = text;
-    //convert to angular:
-
-  }
   getQuote() {
     this.generateQuoteService.getRandomQuote().subscribe((quote: string) => {
-      this.displayText = quote.split('')
-      this.wordArray = quote.split(' ')
+
+      const quoteObject:IQuoteObject = {
+        indexProgress: 0,
+        userProgress: '',
+        charArray: quote.split(''),
+        wordArray: quote.split(' ')
+      };
+  
+      this.quoteObject = quoteObject;
     })
   }
 
