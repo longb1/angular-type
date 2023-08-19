1cee17be3adbcc2160d795d3521d6fba7799cae0
Long Bui <Long.Bui@willistowerswatson.com>
2023-08-19 14:29:55 +0100

    revamp for object

--- rockClimberApp/src/app/game-interface/text-input/text-input.component.ts
+++ rockClimberApp/src/app/game-interface/text-input/text-input.component.ts
@@ -3,7 +3,7 @@ import { Component, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-text-input',
   template: `
-    <input type="text" [(ngModel)]="inputText" (input)="emitUserInput()" (keypress)="emitWord($event)">
+    <input type="text" [(ngModel)]="inputText" (input)="emitUserInput()">
   `,
   styles: []
 })
@@ -11,18 +11,10 @@ export class TextInputComponent{
 
   inputText!: string;
   @Output() inputChange = new EventEmitter<string>();
-  @Output() wordToCompare = new EventEmitter<string>();
 
   emitUserInput() {
     this.inputChange.emit(this.inputText);
   }
 
-  emitWord(event: KeyboardEvent) {
-    if (event.key === ' ') {
-      const word: string = this.inputText;
-      this.inputText = '';
-      this.wordToCompare.emit(word);
-    }
-  }
 
 }
--- rockClimberApp/src/app/game-interface/text-screen/text-screen.component.ts
+++ rockClimberApp/src/app/game-interface/text-screen/text-screen.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { IQuoteObject } from 'src/app/quote-object';
 @Component({
   selector: 'app-text-screen',
   template: `
   <div class="textScreen" #textScreen>
     <span
-      *ngFor="let char of textArray; let index = index"
+      *ngFor="let char of quoteObject?.charArray; let index = index"
       [class.incomplete]="!userInput[index]"
       [class.incorrect]="userInput[index] && userInput[index] !== char"
       [class.correct]="userInput[index] === char"
@@ -22,6 +23,6 @@ import { Component, Input } from '@angular/core';
 export class TextScreenComponent{
 
   @Input() userInput!: string[];
-  @Input() textArray?:string[];
+  @Input() quoteObject?:IQuoteObject;
 
 }
