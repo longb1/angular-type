adba6c3bac11f5b04196dc0a68e9e1e7d540657b
Long Bui <Long.Bui@willistowerswatson.com>
2023-09-10 09:21:42 +0100

    stopwatch

--- rockClimberApp/src/app/game-interface/game-interface.component.ts
+++ rockClimberApp/src/app/game-interface/game-interface.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { GenerateQuoteService } from 'src/app/qenerate-quote.service';
 import { IQuoteObject } from '../quote-object';
+import { StopwatchService } from 'src/app/stopwatch.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-game-interface',
@@ -8,7 +11,10 @@ import { IQuoteObject } from '../quote-object';
   styleUrls: ['./game-interface.component.scss']
 })
 export class GameInterfaceComponent {
-  constructor(private generateQuoteService : GenerateQuoteService) { }
+  constructor(
+    private generateQuoteService : GenerateQuoteService,
+    private StopwatchService : StopwatchService
+    ) { }
 
   receivedInput: string[] = [];
   quoteObject: IQuoteObject = {
@@ -16,11 +22,14 @@ export class GameInterfaceComponent {
     completed: ''
   };
 
+  interval: any;
+  time = new Date();
+
   sendInput(text: string) {
     this.receivedInput = text.split('');
   }
 
-  getQuote() {
+  startGame() {
     this.generateQuoteService.getRandomQuote().subscribe((quote: string) => {
 
       const quoteObject:IQuoteObject = {
@@ -30,6 +39,15 @@ export class GameInterfaceComponent {
   
       this.quoteObject = quoteObject;
     })
+
+    //integrate count down before starting game
+    //then, start stop watch and interval only once before blanking out button
+    this.StopwatchService.start()
+    this.interval=setInterval(() => {
+      this.time.setSeconds(this.time.getSeconds()+1);
+    }, 1000);
+
+    //when last word complete, put button back, stop timer, stop interval, record time - if auth'd , add ot account
   }
 
 }
